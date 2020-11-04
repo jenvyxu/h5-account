9a91f2555ada0b50a663152b5d944caace2066fa
xzw <1272117264@qq.com>
2020-11-04 20:06:11 +0800

    提取Nav组件

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import {
   HashRouter as Router,
   Switch,
   Route,
-  Link,
   Redirect
 } from "react-router-dom";
 import styled from 'styled-components';
+import Nav from './components/Nav';
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -17,19 +17,6 @@ const Wrapper = styled.div`
 const Main = styled.div`
   flex-grow: 1;
   overflow: auto;
-`
-
-const Nav = styled.nav`
-  border: 1px solid blue;
-  >ul {
-    display:flex;
-    >li {
-      width: 33.33%;
-      padding: 16px;
-      text-align: center;
-    }
-  }
-  
 `
 
 function App() {
@@ -55,19 +42,7 @@ function App() {
             </Route>
           </Switch>
         </Main>
-        <Nav>
-          <ul>
-            <li>
-              <Link to="/tags">标签页</Link>
-            </li>
-            <li>
-              <Link to="/money">记账页</Link>
-            </li>
-            <li>
-              <Link to="/statistics">统计页</Link>
-            </li>
-          </ul>
-        </Nav>
+        <Nav />
       </Wrapper>
     </Router>
   );
