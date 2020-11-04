893a98bc167251495e1f2c36be0a56fe2047574c
xzw <1272117264@qq.com>
2020-11-04 19:15:50 +0800

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,66 @@
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      hello world
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签页</Link>
+            </li>
+            <li>
+              <Link to="/money">记账页</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计页</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/money">
+            <Money />
+          </Route>
+          <Route path="/statistics">
+            <Statistics />
+          </Route>
+          <Route path="/tags">
+            <Tags />
+          </Route>
+          <Route path="/" exact>
+            <Redirect to='/money' />
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+
+function NoMatch() {
+  return <h2>页面不存在</h2>
+}
+
+function Tags() {
+  return <h2>标签页</h2>;
+}
+
+function Money() {
+  return <h2>记账页</h2>;
+}
+
+function Statistics() {
+  return <h2>统计页</h2>;
 }
 
 export default App;
