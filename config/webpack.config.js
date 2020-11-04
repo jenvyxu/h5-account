30c9fe017a88b8e5d4e599cbf7243c19959b4115
xzw <1272117264@qq.com>
2020-11-04 22:11:35 +0800

    添加icon图标

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -367,6 +367,13 @@ module.exports = function (webpackEnv) {
             // TODO: Merge this config once `image/avif` is in the mime-db
             // https://github.com/jshttp/mime-db
             {
+              test: /\.svg$/,
+              use: [
+                { loader: 'svg-sprite-loader', options: {} },
+                { loader: 'svgo-loader', options: {} }
+              ]
+            },
+            {
               test: [/\.avif$/],
               loader: require.resolve('url-loader'),
               options: {
