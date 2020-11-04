cbb63b7d0d5f56ecf015e17b204ccc3dad0c75c8
xzw <1272117264@qq.com>
2020-11-04 18:46:16 +0800

    delete unused code and files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import './App.css';
-import  Box, { Btn } from 'yyy/a'
 
 function App() {
   return (
     <div className="App">
-      <Box><Btn>hello world</Btn></Box>
+      hello world
     </div>
   );
 }
