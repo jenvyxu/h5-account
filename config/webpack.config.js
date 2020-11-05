3e872aa2dfa9c42b3751dae421a3a7273b3d46fa
xzw <1272117264@qq.com>
2020-11-05 18:59:20 +0800

    svgo-loader配置删除svg的fill属性

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -370,7 +370,13 @@ module.exports = function (webpackEnv) {
               test: /\.svg$/,
               use: [
                 { loader: 'svg-sprite-loader', options: {} },
-                { loader: 'svgo-loader', options: {} }
+                { loader: 'svgo-loader',
+                  options: {
+                    plugins: [
+                      {removeAttrs: {attrs: 'fill'}}
+                      ]
+                  }
+                }
               ]
             },
             {
