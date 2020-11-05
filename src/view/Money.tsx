d3e88a3e14fbda6dc3ff4b9ed9d1261b1e139550
xzw <1272117264@qq.com>
2020-11-05 22:49:18 +0800

    完成标签的新增和选择

--- src/view/Money.tsx
+++ src/view/Money.tsx
@@ -14,15 +14,7 @@ const MoneyLayout = styled(Layout)`
 const Money = () => {
   return (
     <MoneyLayout>
-      <TagsSection>
-        <ol>
-          <li>衣</li>
-          <li>食</li>
-          <li>住</li>
-          <li>行</li>
-        </ol>
-        <button>新增标签</button>
-      </TagsSection>
+      <TagsSection />
       <NoteSection>
         <label>
           <span>备注</span>
--- src/view/Money/TagsSection.tsx
+++ src/view/Money/TagsSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const TagsSection = styled.section`
+const Wrapper = styled.section`
   flex-grow: 1;
   display: flex;
   flex-direction: column;
@@ -17,6 +18,9 @@ const TagsSection = styled.section`
       background: #d9d9d9;
       border-radius: 18px;
       margin: 8px 12px;
+      &.selected {
+        background:yellow;
+      }
     }
   }
   >button {
@@ -29,4 +33,36 @@ const TagsSection = styled.section`
     margin-top: 8px;
   }
 `
+
+const TagsSection: React.FC = () => {
+  const [tags, setTags] = useState<string[]>(['衣','食','住','行'])
+  const [selectedTags, setSelectedTags] = useState<string[]>([])
+  const onAddTag = () => {
+    const tagName = window.prompt('请输入标签')
+    if(tagName !== null) {
+      setTags([...tags ,tagName])
+    }
+  }
+  const onToggleTag = (tag:string) => {
+    const index = selectedTags.indexOf(tag)
+    // 新增或者移除某个标签
+    if(index>=0) {
+      setSelectedTags(selectedTags.filter(t => t!==tag))
+    } else {
+      setSelectedTags([...selectedTags, tag])
+    }
+  }
+  const getClass = (tag:string) => selectedTags.indexOf(tag) >=0 ? 'selected' : ''
+  return (
+    <Wrapper>
+      <ol>
+        {tags.map(tag =>
+          <li key={tag} onClick={()=> {onToggleTag(tag)}} className={getClass(tag)}>{tag}</li>
+        )}
+      </ol>
+      <button onClick={onAddTag}>新增标签</button>
+    </Wrapper>
+  )
+}
+
 export { TagsSection }
