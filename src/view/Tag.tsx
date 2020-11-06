163d767f144ce664b4d1c43f7d2729fc71443b05
xzw <1272117264@qq.com>
2020-11-07 07:06:05 +0800

    完成Tag组件样式

--- src/view/Tag.tsx
+++ src/view/Tag.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import {useTags} from 'useTags';
+import Layout from 'components/Layout';
+import Icon from 'components/Icon';
+import {Button} from 'components/Button';
+import styled from 'styled-components';
+import {Input} from 'components/Input';
+import { Center } from 'components/Center';
+import { Space } from 'components/Space';
+
+const Topbar = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  line-height: 20px;
+  padding: 14px;
+  background: #fff;
+ `
+const InputWrapper = styled.div`
+  padding: 0 14px;
+  background: #fff;
+  margin-top: 8px;
+`
 
 type Params = {
   id: string
@@ -10,7 +31,22 @@ const Tag = () => {
   const { findTag } = useTags()
   const tag = findTag(parseInt(id))
   return (
-    <div>{tag.name}</div>
+    <Layout>
+      <Topbar>
+        <Icon name="arrow-left" />
+        <span>编辑标签</span>
+        <Icon />
+      </Topbar>
+      <InputWrapper>
+        <Input label="标签名" placeholder="标签名" type="text" value={tag.name}/>
+      </InputWrapper>
+      <Center>
+        <Space />
+        <Space />
+        <Space />
+        <Button>删除标签</Button>
+      </Center>
+    </Layout>
   )
 }
 
