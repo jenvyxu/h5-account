3825dd0ed2ec03e346a9da0cbe2216234d2333ec
xzw <1272117264@qq.com>
2020-11-07 05:17:44 +0800

    修復tag id不連續bug

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,13 +1,15 @@
 import {useState} from 'react';
 import {createId} from 'lib/cteateId';
 
+const defaultTags = [
+  { id: createId(), name: '衣'},
+  { id: createId(), name: '食'},
+  { id: createId(), name: '住'},
+  { id: createId(), name: '行'},
+]
+
 const useTags = () => {
-  const [tags, setTags] = useState<{ id: number, name: string }[]>([
-    { id: createId(), name: '衣'},
-    { id: createId(), name: '食'},
-    { id: createId(), name: '住'},
-    { id: createId(), name: '行'},
-  ]);
+  const [tags, setTags] = useState<{ id: number, name: string }[]>(defaultTags);
   return {tags, setTags};
 }
 
