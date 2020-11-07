df9a0abd7a1277da05b2869ba744387071bb7674
xzw <1272117264@qq.com>
2020-11-07 10:36:18 +0800

    Icon组件添加onClick和className属性

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import cs from 'classnames'
 // require 一个文件夹
 let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
 try {importAll(require.context('assets/icons', true, /\.svg$/));} catch (error) {console.log(error);}
 
 type Props = {
   name?: string
-}
+} & React.SVGAttributes<SVGElement>
 
 const Icon = (props: Props) => {
+  const { name, className, children, ...rest} = props
   return (
-      <svg className="icon">
-        { props.name && <use xlinkHref={'#' + props.name } />}
+      <svg className={cs('icon', className)} {...rest}>
+        { name && <use xlinkHref={'#' + name } />}
       </svg>
   )
 }
