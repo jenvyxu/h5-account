b778bc0d36c16c0d5236ff988d4755a04f544e39
xzw <1272117264@qq.com>
2020-11-14 15:44:35 +0800

    修改记账页新增标签样式

--- src/view/Money/TagsSection.tsx
+++ src/view/Money/TagsSection.tsx
@@ -7,15 +7,16 @@ import {Link} from 'react-router-dom';
 const Wrapper = styled.section`
   flex-grow: 1;
   display: flex;
+  overflow: auto;
   flex-direction: column;
-  justify-content: flex-end;
+  justify-content: space-between;
   align-items: flex-start;
   background-color: #f9faf5;
-  padding: 12px 16px;
+  padding: 4px 16px 12px 16px;
   >ol {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: flex-start;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-start;
     >li {
       width: calc(20% - 8px);
       margin: 4px 4px 0 4px;
@@ -25,19 +26,26 @@ const Wrapper = styled.section`
       display: inline-flex;
       flex-direction: column;
       align-items: center;
-      justify-content: center;
+      justify-content: flex-start;
       font-size: 12px;
       text-align: center;
       >.icon {
         width: 40px;
         height: 40px;
       }
+      >a{
+        >.icon {
+          width: 40px;
+          height: 40px;
+          fill: #f2c841;
+        }
+      }
       &.selected {
         background: #a0ded1;
         border: 2px solid #95c8be;
       }
       &:last-child {
-      margin-right: auto;
+        margin-right: auto;
       }
     }
   }
@@ -49,6 +57,7 @@ const Wrapper = styled.section`
     color: #666;
     outline: none;
     margin-top: 8px;
+    justify-self: flex-end;
   }
 `
 type Props = {
@@ -79,8 +88,8 @@ const TagsSection: React.FC<Props> = (props) => {
               className={getClass(tag.id)}>
             <Icon name={tag.icon}/><span>{tag.name}</span></li>
         )}
+        <li><Link to='/tags/add'><Icon name="add"/></Link></li>
       </ol>
-      <Link to='/tags/add'>新增标签</Link>
     </Wrapper>
   )
 }
