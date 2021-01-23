b76f39f22f188c4095674b7844741ca058ceb51f
xzw <1272117264@qq.com>
2021-01-23 23:23:06 +0800

    perf: 修改默认标签的id

--- src/redux/reducers/tagSlice.tsx
+++ src/redux/reducers/tagSlice.tsx
@@ -5,23 +5,23 @@ import { Tag, TagList } from '../types/tagTypes';
 import { Category } from '../types/categoryTypes';
 
 const initialState: TagList = [
-  { id: 1, name: '房租', icon: 'fangzu', category: 'cost' },
-  { id: 2, name: '水电', icon: 'shuidian', category: 'cost' },
-  { id: 3, name: '居家', icon: 'shop', category: 'cost' },
-  { id: 4, name: '交通', icon: 'train', category: 'cost' },
-  { id: 5, name: '学习', icon: 'book', category: 'cost' },
-  { id: 6, name: '日用', icon: 'riyong', category: 'cost' },
-  { id: 7, name: '餐饮', icon: 'canyin', category: 'cost' },
-  { id: 8, name: '购物', icon: 'shopping', category: 'cost' },
-  { id: 9, name: '娱乐', icon: 'youxi', category: 'cost' },
-  { id: 10, name: '旅游', icon: 'lvyou', category: 'cost' },
-  { id: 11, name: '宠物', icon: 'chongwu', category: 'cost' },
-  { id: 12, name: '工资', icon: 'gongji', category: 'income' },
-  { id: 13, name: '报销', icon: 'baoxiao', category: 'income' },
-  { id: 14, name: '补助', icon: 'buzhu', category: 'income' },
-  { id: 15, name: '红包', icon: 'hongbao', category: 'income' },
-  { id: 16, name: '分红', icon: 'fenhong', category: 'income' },
-  { id: 17, name: '奖金', icon: 'jiangjin', category: 'income' },
+  { id: 0, name: '房租', icon: 'fangzu', category: 'cost' },
+  { id: 1, name: '水电', icon: 'shuidian', category: 'cost' },
+  { id: 2, name: '居家', icon: 'shop', category: 'cost' },
+  { id: 3, name: '交通', icon: 'train', category: 'cost' },
+  { id: 4, name: '学习', icon: 'book', category: 'cost' },
+  { id: 5, name: '日用', icon: 'riyong', category: 'cost' },
+  { id: 6, name: '餐饮', icon: 'canyin', category: 'cost' },
+  { id: 7, name: '购物', icon: 'shopping', category: 'cost' },
+  { id: 8, name: '娱乐', icon: 'youxi', category: 'cost' },
+  { id: 9, name: '旅游', icon: 'lvyou', category: 'cost' },
+  { id: 10, name: '宠物', icon: 'chongwu', category: 'cost' },
+  { id: 11, name: '工资', icon: 'gongji', category: 'income' },
+  { id: 12, name: '报销', icon: 'baoxiao', category: 'income' },
+  { id: 13, name: '补助', icon: 'buzhu', category: 'income' },
+  { id: 14, name: '红包', icon: 'hongbao', category: 'income' },
+  { id: 15, name: '分红', icon: 'fenhong', category: 'income' },
+  { id: 16, name: '奖金', icon: 'jiangjin', category: 'income' },
   { id: 18, name: '借款', icon: 'jiekuan', category: 'income' },
 ]
 
@@ -55,7 +55,7 @@ export const asyncAddTag = createAsyncThunk<Tag, TagArgs, { state: RootState }>(
 export const fetchTagList = createAsyncThunk(
   'tag/fetchTagList',
   async () => {
-    const {tagList} = await httpGetTag()
+    const { tagList } = await httpGetTag()
     return tagList
   }
 )
